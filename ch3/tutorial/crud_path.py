66af7686696d00a46f15e8c87537409c483599ef
datawhales <datawhales@gmail.com>
2022-11-11 18:09:33 +0900

    :sparkles: Add crud api

---
+++ ch3/tutorial/crud_path.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, HTTPException
+
+# Create a FastAPI instance
+app = FastAPI()
+
+# User database
+USER_DB = {}
+
+NAME_NOT_FOUND = HTTPException(status_code=400, detail="Name not found.")
+
+
+@app.post("/users/name/{name}/nickname/{nickname}")
+def create_user(name: str, nickname: str):
+    USER_DB[name] = nickname
+    return {"status": "success"}
+
+
+@app.get("/users/name/{name}")
+def read_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    return {"nickname": USER_DB[name]}
+
+
+@app.put("/users/name/{name}/nickname/{nickname}")
+def update_user(name: str, nickname: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    USER_DB[name] = nickname
+    return {"status": "success"}
+
+
+@app.delete("/users/name/{name}")
+def delete_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    USER_DB.pop(name)
+    return {"status": "success"}
---
+++ ch3/tutorial/crud_pydantic.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class CreateIn(BaseModel):
+    name: str
+    nickname: str
+
+
+class CreateOut(BaseModel):
+    status: str
+    id: int
+
+
+# Create a FastAPI instance
+app = FastAPI()
+
+# User database
+USER_DB = {}
+
+NAME_NOT_FOUND = HTTPException(status_code=400, detail="Name not found.")
+
+
+@app.post("/users", response_model=CreateOut)
+def create_user(user: CreateIn):
+    USER_DB[user.name] = user.nickname
+    user_dict = user.dict()
+    user_dict["status"] = "success"
+    user_dict["id"] = len(USER_DB)
+    return user_dict
+
+
+@app.get("/users")
+def read_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    return {"nickname": USER_DB[name]}
+
+
+@app.put("/users")
+def update_user(name: str, nickname: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    USER_DB[name] = nickname
+    return {"status": "success"}
+
+
+@app.delete("/users")
+def delete_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    USER_DB.pop(name)
+    return {"status": "success"}
