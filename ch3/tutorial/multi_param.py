6f38bb7c29e2965930e1ab7569a5bf6a993353f5
datawhales <datawhales@gmail.com>
2022-11-15 16:13:41 +0900

    :art: Formatting

--- ch3/tutorial/multi_param.py
+++ ch3/tutorial/multi_param.py
@@ -7,9 +7,7 @@ app = FastAPI()
 
 
 @app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
-):
+async def read_user_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
     item = {"item_id": item_id, "owner_id": user_id}
     if q:
         item.update({"q": q})
