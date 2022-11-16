8fa4e03f62833976eba27ab03083920c619d667c
Aiden-Jeon <aiden.jongseob@gmail.com>
2022-11-16 13:41:49 +0900

    add scikit-learn pipeline
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

--- ch2/base_train.py
+++ ch2/base_train.py
@@ -2,20 +2,25 @@
 
 import joblib
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 # 1. get data
 X, y = load_iris(return_X_y=True, as_frame=True)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
 
 # 2. model development and train
-rf = RandomForestClassifier()
-rf.fit(X_train, y_train)
+scaler = StandardScaler()
+classifier = SVC()
 
-train_pred = rf.predict(X_train)
-valid_pred = rf.predict(X_valid)
+scaled_X_train = scaler.fit_transform(X_train)
+scaled_X_valid = scaler.transform(X_valid)
+classifier.fit(scaled_X_train, y_train)
+
+train_pred = classifier.predict(scaled_X_train)
+valid_pred = classifier.predict(scaled_X_valid)
 
 train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
 valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
@@ -24,4 +29,5 @@ print("Train Accuracy :", train_acc)
 print("Valid Accuracy :", valid_acc)
 
 # 3. save model
-joblib.dump(rf, "rf.joblib")
+joblib.dump(scaler, "scaler.joblib")
+joblib.dump(classifier, "classifier.joblib")
