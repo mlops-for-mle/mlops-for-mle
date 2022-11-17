860b3c1f8cc3f2e2b49c63b04c55fbcdbc764690
Jongseob Jeon <aiden.jongseob@gmail.com>
2022-11-17 12:56:13 +0900

    Chapter2. Model Developement (#7)
    * add chapter 3 code
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * update with new format
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * add scikit-learn pipeline
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * update
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * add makefile
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * add run-dependency
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>
    
    * Update ch2/db_train.py
    
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

---
+++ ch2/db_train.py
@@ -0,0 +1,36 @@
+# db_train.py
+
+import joblib
+import pandas as pd
+import psycopg2
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+# 1. get data
+db_connect = psycopg2.connect(host="localhost", database="mydatabase", user="myuser", password="mypassword")
+df = pd.read_sql("SELECT * FROM iris_data ORDER BY id DESC LIMIT 100", db_connect)
+X = df.drop(["id", "target"], axis="columns")
+y = df["target"]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
+
+# 2. model development and train
+model_pipeline = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
+model_pipeline.fit(X_train, y_train)
+
+train_pred = model_pipeline.predict(X_train)
+valid_pred = model_pipeline.predict(X_valid)
+
+train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
+valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
+
+print("Train Accuracy :", train_acc)
+print("Valid Accuracy :", valid_acc)
+
+# 3. save model
+joblib.dump(model_pipeline, "db_pipeline.joblib")
+
+# 4. save data
+df.to_csv("data.csv", index=False)
---
+++ ch2/pipeline_train.py
@@ -0,0 +1,29 @@
+# pipeline_train.py
+
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+# 1. get data
+X, y = load_iris(return_X_y=True, as_frame=True)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
+
+# 2. model development and train
+model_pipeline = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
+model_pipeline.fit(X_train, y_train)
+
+train_pred = model_pipeline.predict(X_train)
+valid_pred = model_pipeline.predict(X_valid)
+
+train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
+valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
+
+print("Train Accuracy :", train_acc)
+print("Valid Accuracy :", valid_acc)
+
+# 3. save model
+joblib.dump(model_pipeline, "model_pipeline.joblib")
