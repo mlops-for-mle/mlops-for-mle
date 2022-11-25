8b81f57c894ec10a2ef49ade0c41d61f79027596
Kimdongui <dh.kim@makinarocks.ai>
2022-11-25 16:05:09 +0900

    :hammer: update ch3 download model

--- ch3/download_model.py
+++ ch3/download_model.py
@@ -7,28 +7,26 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 # 0. set mlflow environments
-MLFLOW_TRACKING_URI = "http://localhost:5000"
-MLFLOW_S3_ENDPOINT_URL = "http://localhost:9000"
-
-os.environ["MLFLOW_S3_ENDPOINT_URL"] = MLFLOW_S3_ENDPOINT_URL
-os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
 os.environ["AWS_ACCESS_KEY_ID"] = "minio"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
 
-# 1. get data
-df = pd.read_csv("data.csv")
-
-X = df.drop(["id", "target"], axis="columns")
-y = df["target"]
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
-
-# 2. load model from mlflow
+# 1. load model from mlflow
 parser = ArgumentParser()
 parser.add_argument("--model-name", dest="model_name", type=str, default="sk_model")
 parser.add_argument("--run-id", dest="run_id", type=str)
 args = parser.parse_args()
 
-model_pipeline = mlflow.sklearn.load_model(f"runs:/{args.run_id}/{args.model_name}")
+model_pipeline = mlflow.pyfunc.load_model(f"runs:/{args.run_id}/{args.model_name}")
+print(model_pipeline)
+
+# 2. get data
+df = pd.read_csv("data.csv")
+
+X = df.drop(["id", "target"], axis="columns")
+y = df["target"]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
 
 # 3. predict results
 train_pred = model_pipeline.predict(X_train)
