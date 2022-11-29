c88ace8eebba0c1b48a6783c7d3c4ac0e6c0313a
dongminlee94 <kid33629@gmail.com>
2022-11-29 09:21:45 +0900

    Add data generator

---
+++ ch7/data_generator.py
@@ -0,0 +1,72 @@
+# data_generator.py
+import time
+
+import pandas as pd
+import psycopg2
+from sklearn.datasets import load_iris
+
+
+def get_data():
+    X, y = load_iris(return_X_y=True, as_frame=True)
+    df = pd.concat([X, y], axis="columns")
+    rename_rule = {
+        "sepal length (cm)": "sepal_length",
+        "sepal width (cm)": "sepal_width",
+        "petal length (cm)": "petal_length",
+        "petal width (cm)": "petal_width",
+    }
+    df = df.rename(columns=rename_rule)
+    return df
+
+
+def create_table(db_connect):
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS iris_data (
+        id SERIAL PRIMARY KEY,
+        sepal_length float8,
+        sepal_width float8,
+        petal_length float8,
+        petal_width float8,
+        target int
+    );"""
+    print(create_table_query)
+    with db_connect.cursor() as cur:
+        cur.execute(create_table_query)
+        db_connect.commit()
+
+
+def insert_data(db_connect, data):
+    insert_row_query = f"""
+    INSERT INTO iris_data
+        (sepal_length, sepal_width, petal_length, petal_width, target)
+        VALUES (
+            {data.sepal_length},
+            {data.sepal_width},
+            {data.petal_length},
+            {data.petal_width},
+            {data.target}
+        );
+    """
+    print(insert_row_query)
+    with db_connect.cursor() as cur:
+        cur.execute(insert_row_query)
+        db_connect.commit()
+
+
+def generate_data(db_connect, df):
+    while True:
+        insert_data(db_connect, df.sample(1).squeeze())
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    db_connect = psycopg2.connect(
+        user="sourceuser",
+        password="sourcepassword",
+        host="source-db",
+        port=5432,
+        database="sourcedatabase",
+    )
+    create_table(db_connect)
+    df = get_data()
+    generate_data(db_connect, df)
