c96f0dceb5e6db23c5d5d8b8e33f577b6b27663d
datawhales <datawhales@gmail.com>
2022-11-30 15:45:32 +0900

    :sparkles: Update 1. API Serving & 2. Docker with Model

--- ch6/app.py
+++ ch6/app.py
@@ -7,20 +7,7 @@ from schemas import PredictIn, PredictOut
 
 
 def get_model():
-    # Set environments
-    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
-    os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
-    os.environ["AWS_ACCESS_KEY_ID"] = "minio"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
-
-    # Get run info
-    df = mlflow.search_runs(max_results=1, experiment_names=["new-exp"])
-
-    # Get run_id
-    run_id = df["run_id"][0]
-
-    # Load model
-    model = mlflow.pyfunc.load_model(model_uri=f"runs:/{run_id}/sk_model")
+    model = mlflow.sklearn.load_model(model_uri="./sk_model")
     return model
 
 
