db2be3d47126737d7e54b5d53c378aebd2f0eb3b
Seokgi Kim <67447280+datawhales@users.noreply.github.com>
2022-12-02 11:44:18 +0900

    ✨ Feature/ch6 deployment (#15)
    * :sparkles: Feature/ch6-model-deployment
    
    * Modified Makefile
    
    * :hammer: modify data insert period
    
    * :art: Apply pr comments
    
    * Add minor changes
    
    * :sparkles: Add model-in-service
    
    * :sparkles: Add docker without model code
    
    * :sparkles: Add docker env command
    
    * :sparkles: Update 1. API Serving & 2. Docker with Model
    
    * Add model dir to .gitignore
    
    * Apply PR comments
    
    Signed-off-by: datawhales <datawhales@gmail.com>
    
    * :art: Apply PR comment
    
    Signed-off-by: datawhales <datawhales@gmail.com>
    
    Signed-off-by: datawhales <datawhales@gmail.com>

---
+++ ch6/download_model.py
@@ -0,0 +1,24 @@
+import os
+from argparse import ArgumentParser
+
+import mlflow
+
+# Set environments
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
+
+
+def download_model(args):
+    # Download model artifacts
+    mlflow.artifacts.download_artifacts(artifact_uri=f"runs:/{args.run_id}/{args.model_name}", dst_path=".")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model-name", dest="model_name", type=str, default="sk_model")
+    parser.add_argument("--run-id", dest="run_id", type=str)
+    args = parser.parse_args()
+
+    download_model(args)
