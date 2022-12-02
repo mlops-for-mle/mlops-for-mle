5378b777e33a1e8e846719b73d47e0f6d0ad2d53
datawhales <datawhales@gmail.com>
2022-12-02 11:38:37 +0900

    :art: Apply PR comment
    Signed-off-by: datawhales <datawhales@gmail.com>

--- ch6/download_model.py
+++ ch6/download_model.py
@@ -3,14 +3,14 @@ from argparse import ArgumentParser
 
 import mlflow
 
+# Set environments
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
+
 
 def download_model(args):
-    # Set environments
-    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
-    os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
-    os.environ["AWS_ACCESS_KEY_ID"] = "minio"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
-
     # Download model artifacts
     mlflow.artifacts.download_artifacts(artifact_uri=f"runs:/{args.run_id}/{args.model_name}", dst_path=".")
 
