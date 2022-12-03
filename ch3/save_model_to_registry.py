bd9e82875df151650b7f8ef6f62e4759a4bbee0a
Kimdongui <dh.kim@makinarocks.ai>
2022-12-03 14:38:41 +0900

    :hammer: change mlflow-server port 5000 -> 5001

--- ch3/save_model_to_registry.py
+++ ch3/save_model_to_registry.py
@@ -12,7 +12,7 @@ from sklearn.svm import SVC
 
 # 0. set mlflow environments
 os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
-os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5001"
 os.environ["AWS_ACCESS_KEY_ID"] = "minio"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "miniostorage"
 
